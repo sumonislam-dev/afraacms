82f7115781bbcbc46b7d40381fa7e974411f169a
Sumon <eubsumon@gmail.com>
2026-08-04 22:34:44 +0600

    fix: gallery zoom issue and next, previous issue solved

--- resources/views/components/frontend/lightbox-gallery.blade.php
+++ resources/views/components/frontend/lightbox-gallery.blade.php
@@ -15,7 +15,7 @@
 >
     <div class="grid grid-cols-2 gap-4 sm:grid-cols-3 lg:grid-cols-4">
         @foreach ($items as $index => $item)
-            <button type="button" @click="show({{ $index }})" class="group relative block aspect-square w-full overflow-hidden rounded-lg bg-gray-100">
+            <button type="button" @click="show({{ $index }})" class="group relative block aspect-square w-full cursor-zoom-in overflow-hidden rounded-lg bg-gray-100">
                 @if ($item['type'] === 'image' && $item['image_url'])
                     <img src="{{ $item['image_url'] }}" alt="{{ $item['caption'] ?? '' }}" class="h-full w-full object-cover transition group-hover:opacity-90" loading="lazy">
                 @else
@@ -31,21 +31,21 @@
         @endforeach
     </div>
 
-    <div x-show="open" x-transition style="display: none;" class="fixed inset-0 z-50 flex items-center justify-center bg-black/90 p-4">
-        <button type="button" @click="open = false" aria-label="{{ __('Close') }}" class="absolute right-4 top-4 text-white hover:text-gray-300">
+    <div x-show="open" x-transition style="display: none;" class="fixed inset-0 z-50 flex items-center justify-center bg-black/90 p-4" @click.self="open = false">
+        <button type="button" @click="open = false" aria-label="{{ __('Close') }}" class="absolute right-4 top-4 cursor-pointer text-white hover:text-gray-300">
             <x-icon name="x-mark" class="h-8 w-8" />
         </button>
 
-        <button type="button" @click="prev()" x-show="items.length > 1" aria-label="{{ __('Previous') }}" class="absolute left-4 top-1/2 -translate-y-1/2 text-white hover:text-gray-300">
+        <button type="button" @click="prev()" x-show="items.length > 1" aria-label="{{ __('Previous') }}" class="absolute left-4 top-1/2 -translate-y-1/2 cursor-pointer text-white hover:text-gray-300">
             <x-icon name="chevron-right" class="h-8 w-8 rotate-180" />
         </button>
 
-        <button type="button" @click="next()" x-show="items.length > 1" aria-label="{{ __('Next') }}" class="absolute right-4 top-1/2 -translate-y-1/2 text-white hover:text-gray-300">
+        <button type="button" @click="next()" x-show="items.length > 1" aria-label="{{ __('Next') }}" class="absolute right-4 top-1/2 -translate-y-1/2 cursor-pointer text-white hover:text-gray-300">
             <x-icon name="chevron-right" class="h-8 w-8" />
         </button>
 
         <template x-if="open && items[index]">
-            <div class="max-h-full max-w-4xl" @click.outside="open = false">
+            <div class="max-h-full max-w-4xl">
                 <template x-if="items[index].type === 'image'">
                     <img :src="items[index].image_url" :alt="items[index].caption ?? ''" class="max-h-[80vh] w-full rounded-sm object-contain">
                 </template>
